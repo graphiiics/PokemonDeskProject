4f4c5795e03a58fd0ab937625d13c00ffae05f29
Gael Manzanares <manzanares.gael@gmail.com>
2022-01-08 21:10:25 -0600

    Create Themes.page.js

---
+++ src/pages/Themes.page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Headline from '../components/Header/Headline/Headline.component';
+import Palettes from '../components/ThemesGrid/Palettes.component';
+
+const Themes = () => {
+    return (
+        <div>
+            <Headline title='T H E M E S' />
+            <Palettes/>
+        </div>
+    )
+}
+
+export default Themes
