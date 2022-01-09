6e18960ffaca1e28231f8501f1a84756ed5ea697
Gael Manzanares <manzanares.gael@gmail.com>
2022-01-08 21:08:22 -0600

    Create About.page.js

---
+++ src/pages/About.page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Headline from '../components/Header/Headline/Headline.component';
+import Info from '../components/Info/Info.component';
+
+const About = () => {
+    
+
+    return (
+        <div>
+            <Headline title='A B O U T' />
+            <Info message='Made by @graphiiics'/>
+        </div>
+    )
+}
+
+export default About
