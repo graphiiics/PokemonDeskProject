aa6a557db86cb2f5c6841fd8317a37023242945d
Gael Manzanares <manzanares.gael@gmail.com>
2022-03-11 12:08:07 -0600

    Create Headline.component.jsx

---
+++ src/components/Header/Headline/Headline.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './headline.css'
+
+const Headline = ({title}) => {
+    return (
+        <h1>
+            {title}
+        </h1>
+    )
+}
+
+export default Headline
