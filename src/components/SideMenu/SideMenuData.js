30aeb4f82fff24580f509988db127eed4ec8e38f
Gael Manzanares <manzanares.gael@gmail.com>
2022-03-11 12:13:29 -0600

    Create SideMenuData.js

---
+++ src/components/SideMenu/SideMenuData.js
@@ -0,0 +1,29 @@
+
+
+export const SideMenuData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: 'fa fa-home',
+        cName: 'nav-text'
+    },
+    {
+        title: 'Favorites',
+        path: '/favorites',
+        icon: 'fa fa-heart',
+        cName: 'nav-text'
+    },
+    {
+        title: 'Themes',
+        path: '/themes',
+        icon: 'fa fa-paint-brush',
+        cName: 'nav-text'
+    },
+    {
+        title: 'About',
+        path: '/about',
+        icon: 'fa fa-info-circle',
+        cName: 'nav-text'
+    }
+];
+
